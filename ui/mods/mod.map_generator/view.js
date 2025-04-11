a1d788a04546e24a4bd84d6161e030f07e91942c
EGSP <repsbro@gmail.com>
2025-04-11 23:05:35 +0300

    added view model and fixed tooltips

--- ui/mods/mod.map_generator/view.js
+++ ui/mods/mod.map_generator/view.js
@@ -1,10 +1,16 @@
 define(function (require) {
+    
+    var ViewModel = function () {
+        var self = this;
+
+        self.test_view_model = function () {
+            console.log('test_view_model');
+        }
+    };
+
     return {
         html: require('text!./map_generator.html'),
-        view_model: {
-            test_view_model: function () {
-                console.log('test_view_model');
-            }
-        }
+        // assign view model constructor that need to be instantiated by caller
+        view_model: new ViewModel()
     };
 });
